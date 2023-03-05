e9bb2c140db39b768ef1bbc1e03e936752b2edd9
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2023-03-05 13:51:21 -0300

    se hizo un cambio de archivos y nombre del repositorio

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    include 'header.php';
+?>
+
+    <main class="menu_index separacion-del-menu">
+        <div class="presentacion">
+            <h2>NOSOTROS</h2>
+            <p>
+                “Naiki” empresa familiar que se origino en el 2020, ubicada en el barrio de Boedo, en un principio se dedicaba solo a la venta de ropas en gran variedad, pero ahora abrio un nuevo local el cual se dedicaba unicamente a la venta de zapatillas de marcas reconocidas.
+
+                En esta pagina se puede encontrar algunos de variados calzados que tenemos en el local, entre ellos encontramos modelos de zapatillas como Air Force, Air max, Vapor Max, Jordan Retro, entre las mas conocidas.
+            </p>
+        </div>
+        <div class="imagen">
+            <img src="img/home.png" alt="">
+        </div>
+    </main>
+    
+<?php
+    include 'footer.php';
+?>
