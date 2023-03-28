5a0f9f7d7f13247dba9cf146ed1c81d51422f25b
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2023-03-28 01:44:09 -0300

    Primera entrega

--- main.js
+++ main.js
@@ -1,27 +1,61 @@
-const AgregarProducto = (opcion) =>{
+const AgregarProducto = (opcion) => {
     let condition = true
     let cantidadProductos = 0
     let producto = []
     do {
-        let nombre = prompt("ingresar nombre de "+ opcion)
-        let talle = prompt("ingresar talles disponibles (separdos por coma) de "+ opcion)
-        let realizado = prompt("ingresar donde fue creado")
-        let modelo = prompt("ingresar modelo de "+ opcion)
-        let precio = prompt("ingresar precio de "+ opcion + " " + nombre)
+        let nombre, talle, realizado, modelo, precio
 
-        condition = confirm("Usted ingreso en "+opcion+":\n nombre: "+nombre+"\n talles: "+talle+"\n relizado en: "+realizado+"\n modelo: "+modelo+"\n precio: $"+precio+"\n\n¿ES CORRECTO?")
-        condition = confirm("¿DESEAS SEGUIR AGREGANDO?")
+        nombre = prompt("ingresar nombre de " + opcion)
+        talle = prompt("ingresar talles disponibles (separdos por coma) de " + opcion)
+        realizado = prompt("ingresar donde fue creado")
+        modelo = prompt("ingresar modelo de " + opcion)
+        precio = prompt("ingresar precio de " + opcion + " " + nombre)
+
+        condition = confirm("Usted ingreso en " + opcion + ":\n nombre: " + nombre + "\n talles: " + talle + "\n relizado en: " + realizado + "\n modelo: " + modelo + "\n precio: $" + precio + "\n\n¿ES CORRECTO?")
+
         if (condition === true) {
-            producto[cantidadProductos] = {nombre,talle,realizado,modelo,precio}
+            producto[cantidadProductos] = { nombre: nombre, talle: talle, realizado: realizado, modelo: modelo, precio: precio }
             cantidadProductos++
         }
+        condition = confirm("¿DESEAS SEGUIR AGREGANDO EN "+opcion+"?")
     } while (condition);
 
     console.log(producto)
     return producto
 }
 
-const verificacionOpcion=(valor)=>{
+const MostrarTodosLosProductos = (valor,producto = "") => {
+    let b = producto
+    for (let i = 0; i < valor.length; i++) {
+        b += "(" + (i + 1) + ")" + "\n";
+        for (let j = 0; j < 5; j++) {
+            switch (j) {
+                case 0:
+                    b += "nombre: " + valor[i].nombre
+                    break
+                case 1:
+                    b += "talle: " + valor[i].talle
+                    break;
+                case 2:
+                    b += "realizado: " + valor[i].realizado
+                    break
+                case 3:
+                    b += "modelo: " + valor[i].modelo
+                    break;
+                case 4:
+                    b += "precio: " + valor[i].precio
+                    break;
+                default:
+                    break;
+            }
+            b += "\n"
+        };
+        b += "=====================\n"
+    }
+    return b
+}
+
+const verificacionOpcion = (valor) => {
     switch (valor) {
         case "1":
             return "remera"
@@ -38,7 +72,7 @@ const MostrarOpciones = () => {
         opcion = verificacionOpcion(opcion)
         if (opcion !== undefined) {
             condition = confirm("Usted eligio la opcion:  (" + opcion + ")  ¿Es correcto?\n(1)Remeras\n(2)Zapatillas \nAceptar = si y cancelar = no")
-        }else{
+        } else {
             condition = false
         }
     } while (!condition);
@@ -50,17 +84,17 @@ const MostrarOpciones = () => {
 const nombreUsuario = prompt("Por favor ingresar el  nombre de usuario")
 let confirmar = true
 let productos = []
+let mostrar = "";
 do {
     alert("Bienvenido " + nombreUsuario + " a Mystic")
     let opcion = MostrarOpciones()
-    
+
     productos = AgregarProducto(opcion)
 
-    for (let i = 0; i < productos.length; i++) {
-        for (let j = 0; j < productos[i].length; j++) {
-            alert(productos[i][j])
-        }
-    }
+    mostrar = MostrarTodosLosProductos(productos,"")
+    alert(mostrar)
+    // SI SON MAS DE 4 NO SE MUESTRA EN EL ALERT
+    console.log(mostrar)
     confirmar = confirm("¿Deseas Continuar agregando productos?")
-    alert(productos)
+    alert("¡¡¡ FUERON "+((productos.length))+" PRODUCTOS AGREGADOS CORRECTAMENTE !!!\nFELICIDADES JHON")
 } while (confirmar);
